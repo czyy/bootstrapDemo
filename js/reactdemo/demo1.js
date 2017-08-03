3bf72353d55adc8f657ecaf091c55b270c2351fd
陈圳源 <573400443@qq.com>
2017-08-03 18:33:44 +0800

    demo

--- js/reactdemo/demo1.js
+++ js/reactdemo/demo1.js
@@ -5,7 +5,7 @@
 /*demo1*/
 ReactDOM.render(
     <h1>hello,world!</h1>,
-    document.getElementById("example")
+    document.getElementById("example1")
 );
 
 /*demo2*/
@@ -20,7 +20,7 @@ ReactDOM.render(
     }
             </ul>
     </div>,
-    document.getElementById("example1")
+    document.getElementById("example2")
 );
 
 /*demo3*/
@@ -31,5 +31,60 @@ ReactDOM.render(
             return item;
         })}
     </div>,
-    document.getElementById("example2")
+    document.getElementById("example3")
 );
+
+
+/*demo4*/
+var HelloWorld = React.createClass({
+    render:function(){
+        return <h1>hello,world4!{this.props.name}</h1>;
+    }
+});
+ReactDOM.render(
+    <HelloWorld name="World:fuck you"/>,
+    document.getElementById("example4")
+);
+
+/*demo5*/
+var ChildDemo = React.createClass({
+    render:function(){
+        return <ol>{
+            React.Children.map(this.props.children,function(item){
+                return <li>{item}</li>
+            })
+        }</ol>
+    }
+});
+
+ReactDOM.render(
+    <ChildDemo>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+    </ChildDemo>,
+        document.getElementById("example5")
+);
+
+
+/*demo6*/
+var TitleDemo = React.createClass({
+    getDefaultProps:function(){
+        return {title:123123123,name:11}
+    },
+    propTypes:{title:React.PropTypes.string.isRequired},
+    render:function(){
+        return <div>{this.props.name}</div>
+    }
+});
+var data=123;
+ReactDOM.render(
+    <TitleDemo title={data} />,
+    document.getElementById("example6")
+);
+
+
+/*demo7*/
+/*demo8*/
+/*demo9*/
+/*demo10*/
