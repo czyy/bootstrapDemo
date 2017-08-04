a2a636e7c4babfbd105c91595bd098660360104d
陈圳源 <573400443@qq.com>
2017-08-04 18:11:43 +0800

    demo

--- js/reactdemo/demo1.js
+++ js/reactdemo/demo1.js
@@ -85,6 +85,81 @@ ReactDOM.render(
 
 
 /*demo7*/
+var MyContent = React.createClass({
+    getInitialState:function(){
+        return {name:"123",title:false}
+    },
+    handleClick:function(){
+        this.refs.myinput.focus();
+        this.setState({name:this.state.title});
+        this.setState({title:!this.state.title});
+    },
+
+    render:function(){
+        var text = this.state.title?"place":"don't";
+       return (<div>
+            <input type="text" ref="myinput" value={this.state.name}/>
+            <input type="button" value="focus the input" onClick={this.handleClick}/>
+               <div onClick={this.handleClick}>{text} Click</div>
+        </div>
+           );
+    }
+});
+ReactDOM.render(
+    <MyContent />,
+    document.getElementById("example7")
+)
+
 /*demo8*/
+var ChangeText = React.createClass({
+    getInitialState:function(){
+        return {value:"hello"};
+    },
+    handleClick:function(event){
+        this.setState({value:event.target.value});
+    },
+    render: function(){
+        return (
+            <div>
+                <input type="text" value={this.state.value} onChange={this.handleClick} />
+                    <p>{this.state.value}</p>
+            </div>
+        )
+    }
+});
+ReactDOM.render(
+    <ChangeText />,
+    document.getElementById("example8")
+)
+
 /*demo9*/
+var TimerSet = React.createClass({
+    getInitialState:function(){
+        return {time:0,opacity:1.0};
+    },
+    componentDidMount:function(){
+        this.timer = setInterval(function(){
+            var time = ++this.state.time;
+            var opacity = this.state.opacity;
+            this.setState({time:time});
+            if(opacity>0.1){
+                opacity -= 0.1;
+            }
+            else{
+                opacity=1;
+            }
+            this.setState({opacity:opacity});
+        }.bind(this),100)
+    },
+    render:function(){
+        return (<p style={{opacity:this.state.opacity}}>{this.state.time}</p>
+
+        )
+    }
+});
+ReactDOM.render(
+    <TimerSet />,
+    document.getElementById("example9")
+);
+
 /*demo10*/
